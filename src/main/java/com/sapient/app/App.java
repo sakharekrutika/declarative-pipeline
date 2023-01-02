00dea48d1252671cb8ab42da810026012d9d1056
Krutika Sakhare <krusakha1@publicisgroupe.net>
2023-01-02 12:05:38 +0530

    file added

---
+++ src/main/java/com/sapient/app/App.java
@@ -0,0 +1,15 @@
+package com.sapient.app;
+import com.sapient.model.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Message message=new Message("hello worls");
+        System.out.println( message.getGreeting() );
+    }
+}
---
+++ src/main/java/com/sapient/model/Message.java
@@ -0,0 +1,20 @@
+package com.sapient.model;
+
+public class Message {
+	private String greeting;
+	public Message() {
+		
+	}
+	public String getGreeting() {
+		return greeting;
+	}
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	public Message(String greeting) {
+		super();
+		this.greeting = greeting;
+	}
+	
+
+}
